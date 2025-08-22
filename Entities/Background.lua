20a72a4aa0c8efb5b11d62efd772603563facb2b
Samuel Rufino <samuelrufino0103@gmail.com>
2025-08-22 20:22:30 -0300

    Scroll Background

--- Entities/Background.lua
+++ Entities/Background.lua
@@ -1,21 +1,37 @@
+local Background = {}
 
-background = {
-    Backgroundy = 1
-}
-function background.load()
-   -- other things
-   background = love.graphics.newImage("Assets/PH_Background.png")
+function Background.load()
+    Background.image = love.graphics.newImage("Assets/PH_Background.png")
+    Background.width = Background.image:getWidth()
+    Background.height = Background.image:getHeight()
+    Background.scrollY = 0
 end
 
--- other callbacks
+function Background.update(playerY, dt)
+    -- O background se move baseado na posição do player
+    -- Fator de parallax (0.5 = move metade da velocidade do player)
+    Background.scrollY = playerY * 0.5
+end
 
-function background.draw()
-    for i = 0, love.graphics.getWidth() / background.getWidth() do
-        for j = 0, love.graphics.getHeight() / background:getHeight() do
-            love.graphics.draw(background, i * background:getWidth(), j * background:getHeight())
+function Background.draw()
+    local screenWidth = love.graphics.getWidth()
+    local screenHeight = love.graphics.getHeight()
+    
+    -- Calcula quantas repetições precisamos
+    local tilesX = math.ceil(screenWidth / Background.width) + 1
+    local tilesY = math.ceil(screenHeight / Background.height) + 2
+    
+    -- Calcula offset para scroll infinito
+    local offsetY = Background.scrollY % Background.height
+    
+    -- Desenha tiles do background
+    for i = 0, tilesX do
+        for j = -1, tilesY do
+            local drawX = i * Background.width
+            local drawY = j * Background.height - offsetY
+            love.graphics.draw(Background.image, drawX, drawY)
         end
     end
-    -- draw other things
 end
 
 return Background
--- main.lua
+++ main.lua
@@ -4,36 +4,38 @@ local Platform = require("Entities.Platform")
 local Background = require("Entities.Background")
 local bump = require("Libs/bump/bump")
 
-
 function love.load()
     Input.load()
     Player.load()
     Platform.load()
+    Background.load()
+    
     love.window.setMode(640, 480)
-
+    
     -- Mundo de colisão
     world = bump.newWorld()
     world:add(Player, Player.x, Player.y, Player.Width, Player.Height)
     world:add(Platform, Platform.x, Platform.y, Platform.Width, Platform.Height)
-
-    -- Background
-    bg_Img = love.graphics.newImage("Assets/PH_Background.png")
 end
 
 function love.update(dt)
     Input.update(dt)
     Player.update(dt)
-
-
+    
+    -- Atualiza o background baseado na posição do player
+    Background.update(Player.y, dt)
 end
 
 function love.draw()
-    -- desenha o background considerando a posição Y
-    background.draw()
-
-    Player.draw()
+    -- Desenha o background primeiro (atrás de tudo)
+    Background.draw()
+    
+    -- Desenha os objetos do jogo
     Platform.draw()
-
-    -- debugzinho
-    love.graphics.print("x: " .. Input.dx .. " y: " .. Input.dy, 10, 10)
+    Player.draw()
+    
+    -- Debug
+    love.graphics.setColor(1, 1, 1) -- Garante que o texto seja branco
+    love.graphics.print("Input - x: " .. Input.dx .. " y: " .. Input.dy, 10, 10)
+    love.graphics.print("Player Y: " .. math.floor(Player.y), 10, 30)
 end
